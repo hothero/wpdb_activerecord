b9c567967970c2b9dce75cfde3e8f8849da23be7
HzChris <hzchirs@gmail.com>
2015-09-18 14:08:16 +0800

    user-comment association

--- lib/wpdb_activerecord/comment.rb
+++ lib/wpdb_activerecord/comment.rb
@@ -4,5 +4,6 @@ module WPDB
     self.primary_key =  :comment_ID
 
     belongs_to :post, foreign_key: "comment_post_ID", class_name: WPDB.configuration.post_class
+    belongs_to :user, foreign_key: "user_id", class_name: WPDB.configuration.user_class
   end
 end
