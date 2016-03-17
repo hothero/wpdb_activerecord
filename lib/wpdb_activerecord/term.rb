7c3cac170bc32ed0b94104cc51b189928de0f8bf
HzChris <hzchirs@gmail.com>
2016-03-17 16:37:02 +0800

    add term has_many association with posts

--- lib/wpdb_activerecord/term.rb
+++ lib/wpdb_activerecord/term.rb
@@ -5,6 +5,8 @@ module WPDB
 
     has_one :term_taxonomy, foreign_key: "term_id", class_name: WPDB.configuration.term_taxonomy_class
     has_many :term_taxonomies, foreign_key: "term_id", class_name: WPDB.configuration.term_taxonomy_class
+    has_many :term_relationships, through: :term_taxonomies, foreign_key: "term_taxonomy_id", class_name: WPDB.configuration.term_taxonomy_class
+    has_many :posts, through: :term_relationships, foreign_key: "object_id", class_name: WPDB.configuration.post_class
 
     scope :tag, -> { joins(:term_taxonomy).where("#{WPDB.configuration.term_taxonomy_class.constantize.table_name}.taxonomy = 'post_tag'") }
     scope :category, -> { joins(:term_taxonomy).where("#{WPDB.configuration.term_taxonomy_class.constantize.table_name}.taxonomy = 'category'") }
