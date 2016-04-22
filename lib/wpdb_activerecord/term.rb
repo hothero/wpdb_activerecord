935fad0222cea8f6091faa3f81a50acd0c608cf6
HzChris <hzchirs@gmail.com>
2016-04-22 17:15:52 +0800

    term and parent association

--- lib/wpdb_activerecord/term.rb
+++ lib/wpdb_activerecord/term.rb
@@ -4,7 +4,7 @@ module WPDB
     self.primary_key = :term_id
 
     has_one :term_taxonomy, foreign_key: "term_id", class_name: WPDB.configuration.term_taxonomy_class
-    has_one :parent_category, through: :term_taxonomies, foreign_key: "parent", class_name: WPDB.configuration.term_taxonomy_class
+    has_one :parent, through: :term_taxonomy, foreign_key: "parent", class_name: WPDB.configuration.term_class
     has_many :term_taxonomies, foreign_key: "term_id", class_name: WPDB.configuration.term_taxonomy_class
     has_many :term_relationships, through: :term_taxonomies, foreign_key: "term_taxonomy_id", class_name: WPDB.configuration.term_taxonomy_class
     has_many :posts, through: :term_relationships, foreign_key: "object_id", class_name: WPDB.configuration.post_class
