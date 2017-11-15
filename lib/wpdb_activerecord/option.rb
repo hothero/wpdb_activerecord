6c384871f5ab0adcf59e1d0b35f82038cc882c54
Nick <dawarmwisdom@gmail.com>
2017-11-15 16:05:14 +0800

    add get_option to WPDB::Option

--- lib/wpdb_activerecord/option.rb
+++ lib/wpdb_activerecord/option.rb
@@ -2,5 +2,9 @@ module WPDB
   class Option < ActiveRecord::Base
     self.table_name = "#{WPDB.configuration.prefix}options"
     self.primary_key = :option_id
+
+    def self.get_option(name)
+      self.where(option_name: name).first.option_value
+    end
   end
 end
